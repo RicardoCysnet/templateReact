d79d46ad6fb8bb500c694b43bbaf9a607b874da9
Ricardo Morón <ricardom@cysnet.es>
2021-02-22 10:46:04 +0100

    ESLINT

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWordl from './components/HelloWorld'
+
+
+
+ReactDOM.render(<HelloWordl></HelloWordl>, document.getElementById('app'))
